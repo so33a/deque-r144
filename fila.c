2bc19ce36ec8f472977bb945f6198a071c488f64
r144 <renatinhu_barbosa@hotmail.com>
2016-09-09 00:05:29 -0300

    Update fila.c

--- fila.c
+++ fila.c
@@ -3,7 +3,7 @@
 #include "fila.h"
 
 
-link novoNo(int item, link next) {
+link novoNo(int item, link next, link prev) {
   link t = malloc(sizeof *t);
   if (t == NULL) {
     printf ("Erro no malloc \n");
@@ -11,21 +11,23 @@ link novoNo(int item, link next) {
   }
   t->item = item;
   t->next = next;
+  t->prev = prev;
+  
   return t;
 }
 
 FILA novaFila() {
   FILA f = malloc(sizeof *f);
-  f->maisNovo = f->maisAntigo = NULL;
+  f->last = f->first = NULL;
   return f;
 }
 
 void inserir(FILA f, int e) {
-  if(f->maisAntigo == NULL) {
-    f->maisAntigo = f->maisNovo = novoNo(e, NULL);
+  if(f->first == NULL) {
+    f->first = f->last = novoNo(e, NULL, NULL);
   } else {
-    f->maisNovo->next = novoNo(e, NULL);
-    f->maisNovo = f->maisNovo->next;
+    f->last->next = novoNo(e, NULL, f->last);
+    f->last = f->last->next;
   }
 }
 
@@ -37,22 +39,21 @@ int remover(FILA f){
     return 0;
   }
   
-  x = f->maisAntigo->item;
-  t = f->maisAntigo;
-  f->maisAntigo = f->maisAntigo->next;
- 
-  if(f->maisAntigo == NULL)
-    f->maisNovo = NULL;
+  x = f->first->item;
+  t = f->first;
+  f->first = f->first->next;
+  if(f->first == NULL)
+    f->last = NULL;
 
   free(t);
   return x;
 }
 int filaVazia(FILA f) {
-  return ((f->maisNovo == NULL) || (f->maisAntigo == NULL));
+  return ((f->last == NULL) || (f->first == NULL));
 }
 void imprimirFila(FILA f) {
   link t;
-  for(t = f->maisAntigo; t != NULL; t = t->next) 
+  for(t = f->first; t != NULL; t = t->next) 
     printf ("%d ", t->item);
   printf ("\n");
 }
@@ -60,5 +61,32 @@ void destroiFila(FILA f) {
   while (!filaVazia(f))
     remover(f);
   free(f);
+}
+int removerBack(FILA f)
+{
+  int x;
+  link t;
+  if(filaVazia(f)){
+    printf ("Erro, a fila esta vazia\n");
+    return 0;
+  }
+  x = f->last->item;
+  t = f->last;
+  f->last = f->last->prev;
+  
+  if(f->last == NULL){
+    f->last = NULL;
+  }
+  free(t);
+  
+}
+void InsereFront(FILA f, e)
+{
+  if(f->last == NULL) {
+    f->last = f->first = novoNo(e, NULL, NULL);
+  } else {
+    f->first->prev = novoNo(e, NULL, f->first);
+    f->first = f->first->prev;
+  }
 }
 
