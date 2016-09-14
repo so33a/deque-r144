98b970a536c24a6b942c60a3ca792b92fbea95a5
r144 <renatinhu_barbosa@hotmail.com>
2016-09-14 20:51:33 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -5,22 +5,25 @@ typedef struct node * link;
 struct node {
   int item;
   link next;
+  link prev;
 };
 
 typedef struct {
-  link first;
-  link last;
+  link primeiro;
+  link ultimo;
 } * FILA;
 
 link novoNo(int item, link next, link prev);
 FILA novaFila();
-void inserir(FILA f, int e);
-int remover(FILA f);
+
+void insereDepois(FILA f, int e);
+int removeDepois(FILA f);
+
+void insereAntes(FILA f, int e);
+int removeAntes(FILA f);
+
 void imprimirFila(FILA f);
 void destroiFila(FILA f);
 int filaVazia(FILA f);
-void inserirFront(FILA f, int e);
-int removerBack(FILA f);
 
-
-#endif 
+#endif
